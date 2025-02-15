e5d38be916d8a5e2a102e3f57e81a07cd3dc2866
Shubham Yadav <hackspire.dev@gmail.com>
2025-02-15 21:47:06 +0530

    Transpose Matrix

---
+++ 2D Arrays/TransposeMatrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class TransposeMatrix {
+    public static void DisplayMatrix (int matrix[][]) {
+        for(int i=0 ; i<matrix.length ; i++) {
+            for (int j=0 ; j<matrix[0].length ; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Transpose_Matrix(int Transposed_Matrix [][] , int matrix[][]) {
+        for(int i=0 ; i<matrix.length ; i++) {
+            for (int j=0 ; j<matrix[0].length ; j++) {
+                Transposed_Matrix[j][i] = matrix[i][j] ; 
+            }
+        }
+    }
+
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in) ; 
+        System.out.println("Enter the no. of rows and cols: ");
+        int n = sc.nextInt() ; 
+        int m = sc.nextInt() ; 
+        int matrix[][] = new int [n][m] ;
+        for (int i=0 ; i<matrix.length ; i++) {
+            for (int j=0 ; j<matrix[0].length ; j++) {
+                System.out.println("Enter the element: ") ; 
+                matrix[i][j] = sc.nextInt() ; 
+            }
+        }
+        System.out.println("Matrix created successfully.");
+
+        DisplayMatrix(matrix);
+
+        int Transposed_Matrix [][] = new int[m][n] ; 
+        Transpose_Matrix(Transposed_Matrix, matrix);
+        DisplayMatrix(Transposed_Matrix);
+    }
+}
