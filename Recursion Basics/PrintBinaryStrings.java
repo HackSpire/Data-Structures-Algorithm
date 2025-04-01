c2290bad0e3d56556c489e463d8b9d96a2d39844
Shubham Yadav <hackspire.dev@gmail.com>
2025-04-01 17:02:15 +0530

    Print Binary Strings without consecutive 0's or 1's [Both cases added]

--- Recursion Basics/PrintBinaryStrings.java
+++ Recursion Basics/PrintBinaryStrings.java
@@ -1,6 +1,6 @@
 public class PrintBinaryStrings {
-
-    public static void printBinStrings (int n , int lastPlace , String str) {
+    //Case: (1) printBinaryStrings without consecutive 1's.
+    public static void printBinStrings1 (int n , int lastPlace , String str) {
         //Base Case: 
         if (n == 0) {   //if n==0 then return the string itself.
                         //Either it will be empty or will be the binString.
@@ -10,8 +10,8 @@ public class PrintBinaryStrings {
         /*
         //Kaam [Work]: 
         if (lastPlace == 0) {
-            printBinStrings(n-1,0, str + "0");
-            printBinStrings(n-1, 1, str + "1");
+            printBinStrings1(n-1,0, str + "0");
+            printBinStrings1(n-1, 1, str + "1");
         }
         else{
             printBinStrings(n-1, 0, str + "0");
@@ -21,13 +21,29 @@ public class PrintBinaryStrings {
         //This logic can be shortened to:
         //Kaam [Work]
 
-        printBinStrings(n-1, 0, str + "0");
+        printBinStrings1(n-1, 0, str + "0");
         if (lastPlace == 0) {
-            printBinStrings(n-1, 1, str + "1");
+            printBinStrings1(n-1, 1, str + "1");
         }
 
     }
+    //Case (2): printBinStrings without consecutive 0's.
+    public static void printBinStrings0 (int n, int lastPlace, String str) {
+        //Base Case: (Same)
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+        //Kaam [Work]
+        printBinStrings0(n-1, 1, str + "1");
+        if (lastPlace == 1) {
+            printBinStrings0(n-1,0, str + "0");
+        }
+    }
     public static void main(String[] args) {
-        printBinStrings(3, 0, "");
+        System.out.println("Without consecutive 1's");
+        printBinStrings1(3, 0, "");
+        System.out.println("Without consecutive 0's");
+        printBinStrings0(3,1,"") ;
     }
 }
